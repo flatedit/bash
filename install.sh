a16d4d0fecdd98284e128374591a6fae67676949
tom-sapletta-com <tom@sapletta.com>
2022-05-30 20:08:22 +0200

    documentation

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 curl https://raw.githubusercontent.com/apifork/bash/main/apifork.sh -o apifork
 ./apifork init
 curl https://raw.githubusercontent.com/flatedit/bash/main/flatedit.sh -o flatedit
-./readme init
+./flatedit init
 curl https://raw.githubusercontent.com/apipackage/bash/main/apipackage.sh -o apipackage
 ./apipackage init
 curl https://raw.githubusercontent.com/apidsl/bash/main/apidsl.sh -o apidsl
